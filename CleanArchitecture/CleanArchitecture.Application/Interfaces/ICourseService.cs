85d3fbe92c9e1124cd4df49cedb224e93763df74
Bhavan Kamble <bhavan.Kamble3@gmail.com>
2021-08-21 23:01:52 +0530

    Commit implement of service and repository getcourse

--- CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
+++ CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchitecture.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,6 @@ namespace CleanArchitecture.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<ViewModels.CourseViewModel> GetCourses();
+        CourseViewModel GetCourses();
     }
 }
